efd1dddf28fb79c658e129e708b2a2a205dd7d56
Wenhao Li <69703322+wenhaoli-xmu@users.noreply.github.com>
2025-08-05 14:08:05 +0800

    Update __init__.py

--- src/modifiers/__init__.py
+++ src/modifiers/__init__.py
@@ -1,5 +1,6 @@
 def get_modifier(method: str, model_type):
     if method == 'uiollms':
         if model_type == 'llama':
-            from src.modifiers.modify_llama_uiollms import UIOLLMs, Teacher
-            return Teacher, UIOLLMs
+            from src.modifiers.modify_llama_ccf import CCF, Teacher
+
+            return Teacher, CCF
